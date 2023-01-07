728ce08ff1ec4ff4d3b03bd95d75aaa4cee77c7d
Milestone <games@kallepersson.se>
2023-01-07 18:13:38 +0100

    Add stage 4

--- main.js
+++ main.js
@@ -3,32 +3,39 @@ var running = true;
 var energy = 0;
 var displayedEnergy = -1;
 var mainNode;
+var mainBud;
 var currentOrbit;
 const ENERGY_PER_CYCLE = 1;
+const trainButtonElm = document.querySelector("#train");
+const upgradeButtonElm = document.querySelector("#upgrade");
 const trainButtonLabelElm = document.querySelector("#train span");
 const upgradeButtonLabelElm = document.querySelector("#upgrade span");
 const cursorElm = document.querySelector("#cursor");
+const progressElm = document.querySelector("#progress");
 const BASE_ORBIT_SIZE = 256;
 const BASE_ANIMATION_SPEED = 1.5;
-const ORBIT_SCALE = 0.1;
-const ORBIT_MAX_COUNT_MIN = 5;
-const ORBIT_MAX_COUNT_SCALE = 2;
+const ORBIT_SCALE = 0.7;
+const ORBIT_MAX_COUNT_MIN = 25;
+const ORBIT_MAX_COUNT_SCALE = 1;
 const UNIVERSE_SCALE = 3;
 const LEAVING_TIME_MIN = 15;
 const LEAVING_TIME_MAX = 25;
-const TRAIN_COST = 5;
-const SPEED_COST = 5;
+const TRAIN_COST = 1;
+const SPEED_COST = 2;
 const SPEED_UPGRADE = 0.005;
 const BLOB_ENERGY_BASE = 22;
+const BLOB_SPAWN_TIMER_MIN = 5000;
+const BLOB_SPAWN_TIMER_MAX = 30000;
+const BLOB_MAX_COUNT = 5;
 var speedUpgradeLevel = 1;
 var mouseDown = false;
 var mouseClick = false;
 var shiftKey = false;
 var altKey = false;
 
-const PHASE_1_THRESHOLD = 100;
-const PHASE_2_THRESHOLD = 500;
-const PHASE_3_THRESHOLD = 1000;
+const PHASE_1_THRESHOLD = 200;
+const PHASE_2_THRESHOLD = 1000;
+const PHASE_3_THRESHOLD = 3000;
 const PHASE_4_THRESHOLD = 4000;
 
 const reflow = _ => {void document.body.offsetWidth;}
@@ -39,8 +46,12 @@ const createNode = function() {
 	const elm = document.createElement("div");
 	mainNode = elm;
 	elm.classList = "node";
+	const budElm = document.createElement("div");		
+	budElm.classList.add("bud");
+	elm.appendChild(budElm);
 	createOrbit(elm);
 	document.body.appendChild(elm);
+	mainBud = budElm;
 	return elm;
 }
 
@@ -121,8 +132,8 @@ const createSatellite = function(parentElm) {
 
 const Init = function() {
 	createNode();
-	trainButtonLabelElm.innerText = calculateTrainCost();
-	upgradeButtonLabelElm.innerText = calculateUpgradeCost();
+	trainButtonElm.title = calculateTrainCost();
+	upgradeButtonElm.title = calculateUpgradeCost();
 	document.addEventListener("mousedown", evt => {
 		shiftKey = evt.shiftKey;
 		altKey = evt.altKey;
@@ -134,32 +145,41 @@ const Init = function() {
 		mouseDown = false;
 	});
 	document.addEventListener("mousemove", evt => {
-		cursorElm.style.transform = `translate(${evt.clientX}px, ${evt.clientY}px)`;
+		// cursorElm.style.transform = `translate(${evt.clientX}px, ${evt.clientY}px)`;
 	})
 	document.addEventListener("click", onClick);
 
 	document.addEventListener("animationiteration", evt => {
 		if (!running) return;
-		const elm = evt.target;
-		if (elm.classList.contains("satellite")) {
+		if (evt.target.classList.contains("satellite")) {
 			energy += ENERGY_PER_CYCLE;
-			const satelliteNode = elm.children[0];
-
-			// const speed = parseFloat(elm.style.animationDuration)*0.9999;
-			// elm.style.animationDuration = `${speed}s`;
 		}
 	});
 
+	document.addEventListener("animationend", evt => {
+		if (evt.target.classList.contains("blob-orbit")) {
+			evt.target.classList.add("disappear");
+		}
+	});
 
 	document.addEventListener("transitionend", evt => {
 		if (!running) return;
-		console.log(evt.target);
 		if (evt.target.classList.contains("blob") && evt.target.classList.contains("acquire")) {
 			evt.target.parentElement.remove();
+		} else if (evt.target.classList.contains("blob-orbit") && evt.target.classList.contains("disappear")) {
+			evt.target.remove();
 		}
 	});
 	window.requestAnimationFrame(tick);
+	window.setTimeout(onBlobSpawnTimerCompete, BLOB_SPAWN_TIMER_MIN + Math.random() * (BLOB_SPAWN_TIMER_MAX - BLOB_SPAWN_TIMER_MIN));
 };
+
+const onBlobSpawnTimerCompete = function() {
+	if (document.querySelectorAll("blob-orbit").length < BLOB_MAX_COUNT) {
+		createBlobOrbit();
+	}
+	window.setTimeout(onBlobSpawnTimerCompete, BLOB_SPAWN_TIMER_MIN + Math.random() * (BLOB_SPAWN_TIMER_MAX - BLOB_SPAWN_TIMER_MIN));
+}
 
 const calculateTrainCost = function() {
 	return document.querySelectorAll(".satellite").length * TRAIN_COST;;
@@ -183,7 +203,7 @@ const onClick = function(evt) {
 		} else {
 			createSatellite(currentOrbit);
 		}
-		trainButtonLabelElm.innerText = calculateTrainCost();
+		trainButtonElm.title = calculateTrainCost();
 	} else if (evt.target.id == "upgrade") {
 		const cost = calculateUpgradeCost();
 		if (energy < cost) return;
@@ -193,7 +213,7 @@ const onClick = function(evt) {
 			const speed = parseFloat(satellite.style.animationDuration)*(1-SPEED_UPGRADE*speedUpgradeLevel);
 			satellite.style.animationDuration = `${speed}s`;
 		});
-		upgradeButtonLabelElm.innerText = calculateUpgradeCost();
+		upgradeButtonLabelElm.title = calculateUpgradeCost();
 	}
 }
 
@@ -202,40 +222,113 @@ const tick = function() {
 	if (energy != displayedEnergy && energy > 0) {
 		if (energy > displayedEnergy) displayedEnergy++;
 		if (energy < displayedEnergy) displayedEnergy--;
-		mainNode.dataset.energy = `${displayedEnergy}`;
+		// progressElm.innerText = `${displayedEnergy}`;
 	}
+
+	const trainCost = calculateTrainCost();
+	const upgradeCost = calculateUpgradeCost();
+	
+	trainButtonElm.classList.toggle("enabled", energy >= trainCost);
+	upgradeButtonElm.classList.toggle("enabled", energy >= upgradeCost);
+	trainButtonLabelElm.style.width = `${Math.min(1, energy/trainCost)*100}%`;
+	upgradeButtonLabelElm.style.width = `${Math.min(1, energy/upgradeCost)*100}%`;
 
 	if (!running) return;
 
-	// Phase 1, awakening
-	if (energy < PHASE_1_THRESHOLD) {
-		document.title = "PHASE1";
-		const rg = 1 + (energy/PHASE_1_THRESHOLD)*25;
-		const b = 7 + (energy*10/PHASE_1_THRESHOLD)*33;
-		// const a = Math.max(0.1, energy/PHASE_1_THRESHOLD);
-		const a = 1;
-		mainNode.style.backgroundColor = `rgba(${rg},${rg},${b},${a})`;
-		// console.log(`radial-gradient(rgb(${rg}, ${rg}, ${b}), rgb(0,0,0));`);
-		document.body.style.background = `radial-gradient(rgb(${rg}, ${rg}, ${b}), rgb(0,0,0))`;
-		//document.body.style.background = `radial-gradient(rgb(${rg}, ${rg}, ${b}), rgb(0,0,0));`;
-	}
-	
-	// Phase 2, dancing
-	if (energy > PHASE_1_THRESHOLD && energy < PHASE_2_THRESHOLD) {
-		document.title = "PHASE2";
-		const radius = (50-energy/10);
-		// mainNode.style.borderTopLeftRadius = `${radius}% ${radius}%`;
-		mainNode.style.borderRadius = `${radius}% ${100-radius}% 50% 50% / ${radius}% 50% 50% ${100-radius}%`;
-		if(radius <= 5) {
-			document.querySelectorAll(".satellite-node").forEach(satelliteNode => {
-				satelliteNode.classList.add("collecting");
-				satelliteNode.style.transform = `translate(0, ${parseFloat(satelliteNode.parentElement.style.width)/4}px) scale(0.5)`;
-			});
-		}
-	}
+	const PHASE_1_MIN_R = 10;
+	const PHASE_1_MIN_G = 10;
+	const PHASE_1_MIN_B = 25;
+	const PHASE_1_MIN_A = 1;
 
-	if (energy > PHASE_3_THRESHOLD) {
+	const PHASE_1_MAX_R = 100;
+	const PHASE_1_MAX_G = 100;
+	const PHASE_1_MAX_B = 255;
+	const PHASE_1_MAX_A = 1;
+
+	const PHASE_2_MIN_R = PHASE_1_MAX_R;
+	const PHASE_2_MIN_G = PHASE_1_MAX_G;
+	const PHASE_2_MIN_B = PHASE_1_MAX_B;
+	const PHASE_2_MIN_A = PHASE_1_MAX_A;
+
+	const PHASE_2_MAX_R = 100;
+	const PHASE_2_MAX_G = 100;
+	const PHASE_2_MAX_B = 100;
+	const PHASE_2_MAX_A = 1;
+
+	const PHASE_3_MIN_R = 100;
+	const PHASE_3_MIN_G = 100;
+	const PHASE_3_MIN_B = 0;
+	const PHASE_3_MIN_A = 1;
+
+	const PHASE_3_MAX_R = 100;
+	const PHASE_3_MAX_G = 100;
+	const PHASE_3_MAX_B = 100;
+	const PHASE_3_MAX_A = 1;
+
+
+	if (energy < PHASE_1_THRESHOLD) {
+		// Phase 1, awakening
+		document.title = "PHASE1";
+		document.body.className = "phase1"
+		const bgFade = 0.5;
+		const phaseProgress = energy/PHASE_1_THRESHOLD;
+		const r = PHASE_1_MIN_R + Math.max(PHASE_1_MIN_R, phaseProgress * (PHASE_1_MAX_R - PHASE_1_MIN_R));
+		const g = PHASE_1_MIN_G + Math.max(PHASE_1_MIN_G, phaseProgress * (PHASE_1_MAX_G - PHASE_1_MIN_G));
+		const b = PHASE_1_MIN_B + Math.max(PHASE_1_MIN_B, phaseProgress * (PHASE_1_MAX_B - PHASE_1_MIN_B));
+		const a = PHASE_1_MIN_A + Math.max(PHASE_1_MIN_A, phaseProgress * (PHASE_1_MAX_A - PHASE_1_MIN_A));
+		mainNode.style.backgroundColor = `rgba(${r},${g},${b},${a})`;
+		// console.log(`radial-gradient(rgb(${rg}, ${rg}, ${b}), rgb(0,0,0));`);
+		document.body.style.background = `radial-gradient(rgb(${r*bgFade}, ${g*bgFade}, ${b*bgFade}), rgb(0,0,0))`;
+		//document.body.style.background = `radial-gradient(rgb(${rg}, ${rg}, ${b}), rgb(0,0,0));`;
+		const progressText = `${Math.round(phaseProgress*100)}%`;
+		if (progressElm.innerText != progressText) progressElm.innerText = progressText;
+
+	} else if (energy > PHASE_1_THRESHOLD && energy < PHASE_2_THRESHOLD) {
+		// Phase 2, dancing
+		document.title = "PHASE2";
+		document.body.className = "phase2";
+		const phaseProgress = (energy-PHASE_1_THRESHOLD)/PHASE_2_THRESHOLD;
+		const r = PHASE_2_MIN_R + Math.max(PHASE_2_MIN_R, phaseProgress * (PHASE_2_MAX_R - PHASE_2_MIN_R));
+		const g = PHASE_2_MIN_G + Math.max(PHASE_2_MIN_G, phaseProgress * (PHASE_2_MAX_G - PHASE_2_MIN_G));
+		const b = PHASE_2_MIN_B + Math.max(PHASE_2_MIN_B, phaseProgress * (PHASE_2_MAX_B - PHASE_2_MIN_B));
+		const a = PHASE_2_MIN_A + Math.max(PHASE_2_MIN_A, phaseProgress * (PHASE_2_MAX_A - PHASE_2_MIN_A));
+		const radius = Math.max(5, (50-50*phaseProgress));
+		mainNode.style.backgroundColor = `rgba(${r},${g},${b},${a})`;
+		// mainNode.style.borderTopLeftRadius = `${radius}% ${radius}%`;
+		document.querySelectorAll(".satellite-node.collecting").forEach(satelliteNode => {
+			satelliteNode.classList.remove("collecting");
+			satelliteNode.style.transform = ``;
+		});
+		mainNode.style.borderRadius = `${radius}% ${100-radius}% 50% 50% / ${radius}% 50% 50% ${100-radius}%`;
+		const progressText = `${Math.round(phaseProgress*100)}%`
+		if (progressElm.innerText != progressText) progressElm.innerText = progressText;
+
+	} else if (energy > PHASE_2_THRESHOLD && energy < PHASE_3_THRESHOLD) {
+		// Phase 3, Harvesting
 		document.title = "PHASE3"
+		document.body.className = "phase3"
+		const phaseProgress = (energy-PHASE_2_THRESHOLD)/PHASE_3_THRESHOLD;
+		const r = PHASE_3_MIN_R + Math.max(PHASE_3_MIN_R, phaseProgress * (PHASE_3_MAX_R - PHASE_3_MIN_R));
+		const g = PHASE_3_MIN_G + Math.max(PHASE_3_MIN_G, phaseProgress * (PHASE_3_MAX_G - PHASE_3_MIN_G));
+		const b = PHASE_3_MIN_B + Math.max(PHASE_3_MIN_B, phaseProgress * (PHASE_3_MAX_B - PHASE_3_MIN_B));
+		const a = PHASE_3_MIN_A + Math.max(PHASE_3_MIN_A, phaseProgress * (PHASE_3_MAX_A - PHASE_3_MIN_A));
+
+		mainNode.style.backgroundColor = `rgba(${PHASE_2_MAX_R},${PHASE_2_MAX_G},${PHASE_2_MAX_B},${PHASE_2_MAX_A})`;
+		mainNode.style.borderRadius = `${5}% ${100-5}% 50% 50% / ${5}% 50% 50% ${100-5}%`;
+
+
+		mainBud.style.backgroundColor = `rgba(${r},${g},${b},${a})`;
+		document.querySelectorAll(".satellite-node").forEach(satelliteNode => {
+			satelliteNode.classList.add("collecting");
+			satelliteNode.style.transform = `translate(0, ${parseFloat(satelliteNode.parentElement.style.width)/4}px) scale(0.5)`;
+		});
+		const progressText = `${Math.round(phaseProgress*100)}%`
+		if (progressElm.innerText != progressText) progressElm.innerText = progressText;
+
+	} else if (energy > PHASE_3_THRESHOLD) {
+		// Phase 4, Victory
+		document.title = "PHASE4"
+		document.body.className = "phase4"
 		running = false;
 		document.querySelectorAll(".satellite-node").forEach(satelliteNode => {
 			satelliteNode.classList.remove("collecting");
